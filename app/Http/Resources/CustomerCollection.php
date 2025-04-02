dbbbfa235a6954d0a777b7f313a2f1295195d717
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-04-02 10:52:59 -0300

    Fixed CustomerCollection

--- app/Http/Resources/CustomerCollection.php
+++ app/Http/Resources/CustomerCollection.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class CustomerCollection extends ResourceCollection {
     
     public function toArray(Request $request): array {
-        return [
-            'status' => true,
-            'data' => CustomerResource::collection($this->collection)
-        ];
+        return parent::toArray($request);
     }
 }
